c972aa9e8870a53dd71c49c9a4b2bab6aa6784d1
benibuehler <beni.buehler@bluewin.ch>
2024-03-25 22:07:29 +0100

    push messenger started

---
+++ src/core/service/push_messenger.rs
@@ -0,0 +1,26 @@
+use async_trait::async_trait;
+use tokio::sync::broadcast;
+use crate::core::contracts::errors::GeneralServerError;
+
+#[async_trait]
+pub trait PushMessengerProvider: Send + Sync {
+    async fn push_message(&self, message: &str) -> Result<(), GeneralServerError>;
+}
+struct PushMessenger {
+    broadcast_sender: broadcast::Sender<String>
+}
+impl PushMessenger{
+    fn new() -> Self {
+        let (broadcast_sender, _) = broadcast::channel(10);
+        Self { broadcast_sender }
+    }
+}
+
+#[async_trait]
+impl PushMessengerProvider for PushMessenger {
+    async fn push_message(&self, message: &str) -> Result<(), GeneralServerError> {
+        let _ = self.broadcast_sender.send(message.to_string())?;
+
+        return Ok(())
+    }
+}
