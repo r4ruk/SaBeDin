100f3bb57ddf1c74c683c1a7e95e3c72aa60ce35
benibuehler <beni.buehler@bluewin.ch>
2024-04-17 20:54:36 +0200

    further work on cache implementation

--- src/cache/core/basic_cache.rs
+++ src/cache/core/basic_cache.rs
@@ -1,17 +1,19 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use chrono::{DateTime, Utc};
 use serde_json::Value;
+use time::Duration;
 
 pub struct Cache {
     // cache holding information for an hour
-    short_store: HashMap<String, ( DateTime<Utc>, Value)>,
+    short_store: Arc<Mutex<HashMap<String, ( DateTime<Utc>, Value)>>>,
 
     // store holding information for 8 hours
-    mid_store: HashMap<String, ( DateTime<Utc>, Value)>,
+    mid_store: Arc<Mutex<HashMap<String, ( DateTime<Utc>, Value)>>>,
 
     // store holding information persistently (stored in an intermediate store,
     // loading and storing on Startup/Shutdown)
-    persistent_store: HashMap<String, (DateTime<Utc>, Value)>
+    persistent_store: Arc<Mutex<HashMap<String, (DateTime<Utc>, Value)>>>
 }
 
 enum StoreLifetime {
@@ -31,37 +33,64 @@ impl Cache {
     }
 
     // gets a possible value from the available stores depending on the given key
-    pub fn get(&self, key: &str) -> Option<&Value> {
+    pub fn get(&self, key: &str) -> Option<Value> {
         self.get_from_cache(StoreLifetime::Short, key)
             .or_else(|| self.get_from_cache(StoreLifetime::Mid, key))
             .or_else(|| self.get_from_cache(StoreLifetime::Persistent, key))
     }
 
+    // method adds element to cache (into default store which is short (1h))
+    pub fn add_element(&mut self, item: (String, Value)) {
+        self.add_element_internal(item, StoreLifetime::Short);
+    }
 
-    // short store implementations
+    // method to add element into specific store
+    pub fn add_element_in_specific_store(&mut self, item: (String, Value), store_lifetime: StoreLifetime) {
+        self.add_element_internal(item, store_lifetime);
+    }
 
-
-    // mid store implementations
-
-
-    // persistent store implementations
-
-
-
-    fn get_from_cache(&self, from_lifetime: StoreLifetime, key: &str) -> Option<&Value> {
-        let mut result = match from_lifetime {
-            StoreLifetime::Short => self.short_store.get(key),
-            StoreLifetime::Mid => self.mid_store.get(key),
-            StoreLifetime::Persistent => self.persistent_store.get(key)
-        };
-
-        return match result {
-            Some(value) => {
-                Some(&value.1)
+    // adds element internally into the defined store
+    fn add_element_internal(&mut self, item: (String, Value), store_lifetime: StoreLifetime) {
+        match store_lifetime {
+            StoreLifetime::Short => {
+                self.short_store.lock().unwrap().insert(item.0, (Utc::now(), item.1));
             },
-            None => {
-                None
+            StoreLifetime::Mid => {
+                self.mid_store.lock().unwrap().insert(item.0, (Utc::now(), item.1));
+            },
+            StoreLifetime::Persistent => {
+                self.persistent_store.lock().unwrap().insert(item.0, (Utc::now(), item.1));
             }
         }
+    }
+
+    // gets element from cache
+    fn get_from_cache(&self, from_lifetime: StoreLifetime, key: &str) -> Option<Value> {
+        let result = match from_lifetime {
+            StoreLifetime::Short => self.short_store.lock().unwrap().get(key).cloned(),
+            StoreLifetime::Mid => self.mid_store.lock().unwrap().get(key).cloned(),
+            StoreLifetime::Persistent => self.persistent_store.lock().unwrap().get(key).cloned(),
+        };
+
+        match result {
+            Some(value) =>{
+                let (_, val) = value.clone();
+                Some(val)
+            }
+            None => None,
+        }
+    }
+
+    // start worker to check for elements in cache to clean up
+    fn start_worker(&self) {
+        let store_clone = self.short_store.clone();
+        std::thread::spawn(move || {
+            loop {
+                std::thread::sleep(std::time::Duration::new(1, 0));
+                let mut map = store_clone.lock().unwrap();
+                let current_time = Utc::now();
+                map.retain(|_, (expiry, _)| *expiry > current_time);
+            }
+        });
     }
 }
