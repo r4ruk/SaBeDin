ee8cfbbc2332f8fa3756137ed1158b013368b608
benibuehler <beni.buehler@bluewin.ch>
2024-05-19 20:47:57 +0200

    add waiting timeout on reading queue

--- src/queue_manager/receiver.rs
+++ src/queue_manager/receiver.rs
@@ -1,11 +1,15 @@
 use async_std::stream::StreamExt;
+use axum::routing::get;
 use lapin::{Channel, Consumer};
 use lapin::options::{BasicAckOptions, BasicConsumeOptions, QueueDeclareOptions};
 use lapin::types::FieldTable;
 use uuid::Uuid;
 use crate::core::contracts::errors::GeneralServerError;
 use crate::core::contracts::queue_types::{QueueRequestMessage, QueueResponseMessage};
+use crate::core::contracts::system_messages::InformationMessage;
+use crate::logger::core_logger::{get_logger, LoggingLevel};
 use crate::queue_manager::manager::QueueManager;
+use tokio::time::{timeout, Duration};
 
 /// receives a message from the given queue
 pub async fn receive_on_queue(_manager: &QueueManager , channel: Channel, queue_name: &str)
@@ -53,7 +57,7 @@ pub async fn receive_on_queue(_manager: &QueueManager , channel: Channel, queue_
 pub(crate) async fn establish_temporary_listener(manager: &QueueManager , channel: Channel, correlation_id: Uuid)
                                       -> Result<QueueResponseMessage, GeneralServerError> {
 
-    // TODO Add timeout to decouple from slow or disconnected responder
+    let timeout_duration = Duration::from_secs(15);
 
     let queue_declareoptions = QueueDeclareOptions {
         passive: false,
@@ -64,7 +68,9 @@ pub(crate) async fn establish_temporary_listener(manager: &QueueManager , channe
     };
 
     let queue = manager.create_queue(channel.clone(), &correlation_id.to_string(),queue_declareoptions).await?;
-    println!("Declared queue {:?}", queue);
+    
+    let logger = get_logger();
+    logger.lock().unwrap().log_error(InformationMessage { message: format!("Declared queue {:?}", queue) }, LoggingLevel::Information);
 
     let mut consumer: Consumer = channel
         .basic_consume(
@@ -75,17 +81,50 @@ pub(crate) async fn establish_temporary_listener(manager: &QueueManager , channe
         )
         .await?;
 
-    let mut response_body= String::new();
-
-    while let Some(delivery) = consumer.next().await {
-        if let Ok(delivery) = delivery {
-            println!("received msg: {:?}", delivery);
-            response_body = String::from_utf8_lossy(&delivery.data).to_string();
-            channel
-                .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
-                .await?;
-            return Ok(QueueResponseMessage { correlation_id, body: response_body.to_string()})
+    let result = timeout(timeout_duration, async {
+        while let Some(delivery) = consumer.next().await {
+            if let Ok(delivery) = delivery {
+                println!("received msg: {:?}", delivery);
+                let response_body = String::from_utf8_lossy(&delivery.data).to_string();
+                channel
+                    .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
+                    .await?;
+                return Ok(QueueResponseMessage {
+                    correlation_id: correlation_id.clone(),
+                    body: response_body,
+                });
+            }
         }
-    }
-    return Ok(QueueResponseMessage { correlation_id, body: response_body.to_string() })
+        // Return an error if the loop completes without receiving a message
+        Err(GeneralServerError {
+            message: "No message received".into(),
+        })
+    }).await;
+
+    let ret_result = match result {
+        // all good, return response mapped inside retrieval function
+        Ok(Ok(response)) => Ok(response),
+
+        // Error happened when message was received...
+        Ok(Err(err)) =>{
+            let logger = get_logger();
+            logger.lock().unwrap().log_error(GeneralServerError{
+                message: format!("Internal error in queue message retrieval for correlationId: {}", correlation_id)
+            }, LoggingLevel::Error);
+            Err(err)
+        },
+
+        // Connection timeout reached so returning unsuccessful
+        Err(_) => {
+            let logger = get_logger();
+            logger.lock().unwrap().log_error(GeneralServerError{
+                message: format!("Timeout in queue message retrieval for correlationId: {}", correlation_id)
+            }, LoggingLevel::Error);
+
+            Err(GeneralServerError {
+            message: "Timeout elapsed".into(),
+            })
+        },
+    };
+    return ret_result
 }
