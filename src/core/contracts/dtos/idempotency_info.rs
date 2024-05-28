dcce7b0bb0bcef5f780b3d29a3e70bf5373192f6
benibuehler <beni.buehler@bluewin.ch>
2024-05-28 19:50:15 +0200

    forgotten file

--- src/core/contracts/dtos/idempotency_info.rs
+++ src/core/contracts/dtos/idempotency_info.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use uuid::Uuid;
@@ -8,6 +9,30 @@ pub struct IdempotencyObject {
     #[serde(with="custom_uuid")]
    pub user_id: Uuid,
    pub idempotency_key: String,
-   pub response_status_code: i8,
+   pub response_status_code: i16,
    pub response_body: Value,
+}
+
+#[derive(Debug)]
+pub enum IdempotencyEvents {
+    CreateIdempotencyKey,
+    UpdateIdempotencyKey
+}
+
+impl IdempotencyEvents {
+    pub fn name(&self) -> String{
+        format!("{:?}", self)
+    }
+}
+
+impl FromStr for IdempotencyEvents {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<IdempotencyEvents, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "CreateIdempotencyKey" => Ok(IdempotencyEvents::CreateIdempotencyKey),
+            "UpdateIdempotencyKey" => Ok(IdempotencyEvents::UpdateIdempotencyKey),
+            _ => Err(()),
+        }
+    }
 }
